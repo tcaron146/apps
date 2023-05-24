9f5aede6eba80d655af7ac2050b8e0dad6936ac6
tcaron146 <117337213+tcaron146@users.noreply.github.com>
2023-05-23 23:57:20 -0400

    got dropdown with transition working in bootstrap with a little css, and got node.js working with my project, next step will be content and a database

--- personal_web/server.js
+++ personal_web/server.js
@@ -2,20 +2,6 @@
 const express = require('express');
 const app = express();
 
-// Search route
-app.get('/search', (req, res) => {
-  // Extract the search query from request parameters
-  const query = req.query.query;
-
-  // Perform search logic here (replace with your own implementation)
-  // Example: assuming you have an array of items to search from
-  const items = ['item1', 'item2', 'item3', 'item4'];
-  const searchResults = items.filter(item => item.includes(query));
-
-  // Return the search results as a response
-  res.json({ results: searchResults });
-});
-
 // Start the server
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
