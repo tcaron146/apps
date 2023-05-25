10adfb617e23f7d24f776313b79e0af971d0c873
tcaron146 <117337213+tcaron146@users.noreply.github.com>
2023-05-24 22:31:14 -0400

    so much has happened

---
+++ personal_web/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $("#emailModal").modal("show");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var instagramLink =
+    "https://instagram.com/soupdejourtommy?igshid=NTc4MTIwNjQ2YQ==";
+  document.getElementById("instagram-link").href = instagramLink;
+});
