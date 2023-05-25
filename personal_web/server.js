964b3ac5f88cbe67f82b0eb293a6ced635393626
tcaron146 <117337213+tcaron146@users.noreply.github.com>
2023-05-25 10:02:57 -0400

    working

--- personal_web/server.js
+++
@@ -1,8 +0,0 @@
-// Import required modules and dependencies
-const express = require('express');
-const app = express();
-
-// Start the server
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
