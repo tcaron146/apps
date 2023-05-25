2dec0ddf5e26953f899b214eb3726f826a4ad53c
tcaron146 <117337213+tcaron146@users.noreply.github.com>
2023-05-25 12:34:36 -0400

    craziness

---
+++ personal_web/personal_web/admin.py
@@ -0,0 +1,5 @@
+from .models import PersonalWeb
+
+from django.contrib import admin
+
+admin.site.register(PersonalWeb)
---
+++ personal_web/personal_web/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class PersonalWeb(models.Model):
+    title = models.CharField(max_length = 200)
+    year = models.IntegerField()
+    def __str__(self):
+        return f'{self.title} from {self.year}'
